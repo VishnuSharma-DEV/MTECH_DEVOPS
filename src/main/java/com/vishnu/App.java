745c31fcd1a9644f666cec18387fed53f11af923
Vishnu Sharma 2021wc86523 <vishnusharma.dev@gmail.com>
2025-07-05 21:59:34 +0530

    Added new print Line under java file

--- src/main/java/com/vishnu/App.java
+++ src/main/java/com/vishnu/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins CI!");
 	System.out.println("CI testing is working sucessfully with Poll SCM");
+	System.out.println("New Print Statement to test the Poll SCM");
     }
 }
