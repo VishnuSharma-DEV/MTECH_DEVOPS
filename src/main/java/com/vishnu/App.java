d43fe40db2e371c1f2d96520a0527feaa5bfa668
Vishnu Sharma 2021wc86523 <vishnusharma.dev@gmail.com>
2025-07-05 21:41:53 +0530

    App.java has been updated with Poll SCM

--- src/main/java/com/vishnu/App.java
+++ src/main/java/com/vishnu/App.java
@@ -3,6 +3,6 @@ package com.vishnu;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins CI!");
+	System.out.println("CI testing is working sucessfully with Poll SCM");
     }
 }
-System.out.println("CI working test!");
